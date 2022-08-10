f85bb78504a5f63d2099f3da77814646842ff581
SaurabhPatil1112 <snp44v@gmail.com>
2022-08-10 16:07:05 +0530

    added a header
    added header in signup

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import '../../App.css';
-import { Button } from '../Button';
+import React from "react";
+import "../../App.css";
+import { Button } from "../Button";
 
 export default function SignUp() {
   return (
-    <div className='signup-container'>
-      <div className='input-area'>
-        <form>
-          <input 
-          className='signup-input'
-          name='name'
-          type='name'
-          placeholder='Your Name'
+    <div className="signup-container">
+      <div className="input-area">
+        <h1>Get Started!! Join Now</h1>
+        <form className="form">
+          <input
+            className="signup-input"
+            name="name"
+            type="name"
+            placeholder="Your Name"
           />
-          <input 
-          className='signup-input'
-          name='email'
-          type='email'
-          placeholder='Email ID'
+          <input
+            className="signup-input"
+            name="email"
+            type="email"
+            placeholder="Email ID"
           />
-          <input 
-          className='signup-input'
-          name='phone'
-          type='phone'
-          placeholder='Phone Number'
+          <input
+            className="signup-input"
+            name="phone"
+            type="phone"
+            placeholder="Phone Number"
           />
-          <input 
-          className='signup-input'
-          name='publicid'
-          type='id'
-          placeholder='Your Public ID'
+          <input
+            className="signup-input"
+            name="publicid"
+            type="id"
+            placeholder="Your Public ID"
           />
-          <Button buttonStyle='btn--outline'>SUBMIT</Button>
+          <Button buttonStyle="btn--outline">SUBMIT</Button>
         </form>
       </div>
-
     </div>
-  )
+  );
 }
